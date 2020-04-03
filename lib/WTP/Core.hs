0e3b8c101b6bae933a869be6793ca22f801d7b52
Oskar Wickström <oskar.wickstrom@ingka.com>
2020-04-03 11:27:24 +0200

    Add simplification of syntax tree

--- lib/WTP/Core.hs
+++ lib/WTP/Core.hs
@@ -1,20 +1,22 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
 module WTP.Core where
 
 import Data.Char (Char)
+import Data.Functor (Functor (..))
 import Data.String (IsString)
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Prelude ((<>), ($), (.), Eq, Maybe, Show(..))
-import Data.Functor (Functor(..))
+import Prelude hiding (Bool (..))
 
 newtype Selector = Selector Text
   deriving (Show, IsString, Generic)
@@ -48,29 +50,57 @@ data Query a where
   Map :: (a -> b) -> Query a -> Query b
 
 instance Functor Query where
-    fmap f (Map g q) = Map (fmap f g) q
-    fmap f q = Map f q
+  fmap f (Map g q) = Map (fmap f g) q
+  fmap f q = Map f q
 
 instance Show (Query a) where
-    show = \case
-        Query selector -> "Query " <> show selector
-        QueryAll selector -> "QueryAll " <> show selector
-        Require query -> "Require " <> show query
-        Get attribute query -> "Get " <> show attribute <> " " <> show query
-        Map f query -> "Map _ " <> show query
+  show = \case
+    Query selector -> "(Query " <> show selector <> ")"
+    QueryAll selector -> "(QueryAll " <> show selector <> ")"
+    Require query -> "Require (" <> show query <> ")"
+    Get attribute query -> "(Get " <> show attribute <> " " <> show query <> ")"
+    Map f query -> "(Map _ " <> show query <> ")"
 
-data Formula where
-  True :: Formula
-  Not :: Formula -> Formula
-  Or :: Formula -> Formula -> Formula
-  Until :: Formula -> Formula -> Formula
-  Assert :: Show a => Query a -> Assertion a -> Formula
+data Language = Full | Simplified
 
-deriving instance Show Formula
+data Formula (l :: Language) where
+  -- Simplified language operators
+  True :: Formula l
+  Not :: Formula l -> Formula l
+  Or :: Formula l -> Formula l -> Formula l
+  Until :: Formula l -> Formula l -> Formula l
+  Assert :: Show a => Query a -> Assertion a -> Formula l
+  -- Full language operators
+  False :: Formula Full
+  Eventually :: Formula Full -> Formula Full
+  Always :: Formula Full -> Formula Full
+  And :: Formula Full -> Formula Full -> Formula Full
+  Implies :: Formula Full -> Formula Full -> Formula Full
+  Equivalent :: Formula Full -> Formula Full -> Formula Full
+  Release :: Formula Full -> Formula Full -> Formula Full
 
-data Specification
-  = Specification
+deriving instance Show (Formula l)
+
+simplify :: Formula Full -> Formula Simplified
+simplify = \case
+  -- Derived operators (only present in `Full` language) are simplified
+  False -> Not True
+  Eventually p -> Until True (simplify p)
+  Always p -> Not (simplify (Eventually (Not p)))
+  And p q -> Not (Not (simplify p) `Or` Not (simplify q))
+  Implies p q -> Not (simplify p) `Or` simplify q
+  Equivalent p q -> simplify (p `Implies` q) `Or` simplify (q `Implies` p)
+  Release p q -> Not (Not (simplify p) `Until` Not (simplify q))
+  -- Simplified language operators are preserved
+  True -> True
+  Not p -> Not (simplify p)
+  Or p q -> Or (simplify p) (simplify q)
+  Until p q -> Until (simplify p) (simplify q)
+  Assert query assertion -> Assert query assertion
+
+data Property
+  = Property
       { actions :: [Action],
-        correctness :: Formula
+        specification :: Formula Full
       }
   deriving (Show, Generic)
--- lib/WTP/Syntax.hs
+++ lib/WTP/Syntax.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -16,48 +17,16 @@ import Prelude ((<>), ($), (.), Eq, Maybe, Show(..))
 import Data.Functor (Functor(..))
 import WTP.Core
 
-query :: Selector -> Query (Maybe Element)
-query = Query
-
-queryAll :: Selector -> Query [Element]
-queryAll = QueryAll
-
-require :: Query (Maybe a) -> Query a
-require = Require
-
-get :: Attribute a -> Query Element -> Query a
-get = Get
-
-true, false :: Formula
-true = True
-false = Not True
-
-not, eventually, always :: Formula -> Formula
-not = Not
-eventually = until true
-always = not . eventually . not
-
-or, and, until, implies, equivalent, release :: Formula -> Formula -> Formula
-or = Or
-and p q = not (not p `or` not q)
-until = Until
-implies p q = not p `or` q
-equivalent p q = (p `implies` q) `or` (q `implies` p)
-release p q = not (not p `until` not q)
-
-assert :: Show a => Query a -> Assertion a -> Formula
-assert = Assert
-
 infix 4 \/, /\
 
-(/\), (\/) :: Formula -> Formula -> Formula
-(/\) = and
-(\/) = or
+(/\), (\/) :: Formula Full -> Formula Full -> Formula Full
+(/\) = And
+(\/) = Or
 
 infix 5 ===
 
-(===) :: (Show a, Eq a) => Query a -> a -> Formula
-query === expected = assert query (Equals expected)
+(===) :: (Show a, Eq a) => Query a -> a -> Formula Full
+query === expected = Assert query (Equals expected)
 
 --
 -- EXAMPLE
@@ -65,24 +34,24 @@ query === expected = assert query (Equals expected)
 
 data SpinnerState = Active | Hidden
 
-example :: Specification
+example :: Property
 example =
-  Specification
+  Property
     { actions = [Focus ".my-app input", KeyPress 'x', Click ".my-app button[type=submit]"],
-      correctness =
+      specification =
         ( hasMessage
             "Post a comment below."
             ["message", "info"]
             /\ spinnerIs Hidden
         )
-          `until` spinnerIs Active
-          `until` ( always
+          `Until` spinnerIs Active
+          `Until` ( Always
                       ( hasMessage
                           "Failed to post comment."
                           ["message", "error"]
                           /\ spinnerIs Hidden
                       )
-                      \/ always
+                      \/ Always
                         ( hasMessage
                             "Form posted!"
                             ["message", "error"]
@@ -92,18 +61,18 @@ example =
     }
   where
     spinnerIs state =
-      get
+      Get
         ClassList
-        (require (query ".my-app .spinner"))
+        (Require (Query ".my-app .spinner"))
         === case state of
           Active -> ["spinner", "active"]
           Hidden -> ["spinner"]
     hasMessage message classes =
-      ( get
+      ( Get
           ClassList
-          (require (query ".my-app .message"))
+          (Require (Query ".my-app .message"))
           === classes
       )
-        `and` ( get InnerText (require (query ".my-app .message"))
+        `And` ( Get InnerText (Require (Query ".my-app .message"))
                   === message
               )

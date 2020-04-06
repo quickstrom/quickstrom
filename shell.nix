d5c11546b03d66aa9348a203074337f53d6ce57c
Oskar Wickström <oskar@wickstrom.tech>
2020-04-06 10:56:13 +0200

    Get shell working

--- shell.nix
+++ shell.nix
@@ -3,19 +3,18 @@ let
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: { };
   };
-  ghcide-ghc865 = (import (builtins.fetchTarball
-    "https://github.com/cachix/ghcide-nix/tarball/master") { }).ghcide-${compiler};
+  ghcide = (import (builtins.fetchTarball
+    "https://github.com/cachix/ghcide-nix/tarball/master") { }).${"ghcide-${compiler}"};
 
   wtp = pkgs.haskell.lib.justStaticExecutables (pkgs.haskell.lib.dontHaddock
     (haskellPackages.callCabal2nix "wtp" ./. { }));
 
-in 
-pkgs.haskellPackages.shellFor {
+in haskellPackages.shellFor {
   withHoogle = true;
   packages = p: [wtp];
   buildInputs = (with pkgs;
   [
-    ghcide-ghc865
+    ghcide
     cabal-install
     haskellPackages.ormolu
   ]);

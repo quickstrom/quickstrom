e60b363b3f947a2e6985697bf00a509059734f49
Oskar Wickström <oskar@wickstrom.tech>
2020-05-28 07:04:33 +0200

    Drawn more circles, owl remaining

--- lib/WTP/PureScript.hs
+++ lib/WTP/PureScript.hs
@@ -1,44 +1,88 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE PackageImports #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module WTP.PureScript where
 
-import           Language.PureScript.AST.Literals
-import           Language.PureScript.Comments
-import qualified Language.PureScript.Constants    as PureScript
-import           Language.PureScript.CoreFn
-import           Language.PureScript.Names
-import           Language.PureScript.PSString
+import Protolude
 
-import System.FilePath.Glob (glob)
-import "purescript" Control.Monad.Logger (runLogger')
-import Control.Monad.Trans.Reader (ReaderT(runReaderT))
-import Data.Text (Text)
-import Control.Monad.Error.Class (MonadError(throwError))
-import Control.Monad.Except (ExceptT(ExceptT), runExceptT)
-import Control.Monad.Trans.Class (MonadTrans(lift))
-import Control.Monad.IO.Class (MonadIO(liftIO))
-import Control.Monad.Trans.Writer.Strict (WriterT(runWriterT))
-import WTP.Value
+import Language.PureScript.CoreFn
+import Language.PureScript.Names
+import WTP.Element
+import Data.Scientific
+import Data.HashSet (HashSet)
+import Data.Vector (Vector)
+import Data.Version (Version)
+import qualified Data.Aeson.Types as JSON
+import qualified Data.Aeson as JSON
+import Language.PureScript.CoreFn.FromJSON (moduleFromJSON)
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+data Value
+  = VNull
+  | VBool Bool
+  | VElement Element
+  | VString Text
+  | VFunction Name Value
+  | VVar Name
+  | VNumber Scientific
+  | VSeq (Vector Value)
+  | VSet (HashSet Value)
+  deriving (Eq, Show)
+
 
 data EvalError
+  = Unexpected
+  | InvalidEntryPoint Ident
+  deriving (Eq, Show)
 
-eval :: Module -> Ident -> Either EvalError Module
-eval inputGlobs input = runExceptT $ do
-  inputFiles <- liftIO (concat <$> traverse glob inputGlobs)
-  mods :: [(FilePath, Module)] <- ExceptT $ I.loadAllModules inputFiles
-  lift (print (head mods))
-  case CST.parseModuleFromFile "<file>" input >>= CST.resFull of
-    Left parseError ->
-      throwError $ CST.toMultipleErrors "<file>" parseError
-    Right m -> do
-      flip runReaderT P.defaultOptions $ do
-        ((P.Module ss coms moduleName elaborated exps, _env), _) <- fmap fst . runWriterT . P.runSupplyT 0 $ do
-          desugared <- (P.desugar P.primEnv mempty [P.importPrim m]) >>= \case
-            [d] -> pure d
-            _ -> error "desugaring did not produce one module"
-          P.runCheck' (P.emptyCheckState P.initEnvironment) $ P.typeCheckModule desugared
-        regrouped <- P.createBindingGroups moduleName . P.collapseBindingGroups $ elaborated
-        pure (P.Module ss coms moduleName regrouped exps)
+type Eval a = Except EvalError a
+
+runEval :: Eval a -> (Either EvalError a)
+runEval = runExcept
+
+loadModule :: FilePath -> IO (Either Text (Version, Module Ann))
+loadModule path = do
+  j <- liftIO (BS.readFile path)
+  case JSON.decode j of
+    Just val ->
+      case JSON.parse moduleFromJSON val of
+        JSON.Success m -> pure (Right m)
+        JSON.Error e   -> pure (Left (toS e))
+    Nothing -> pure (Left "Couldn't read CoreFn file.")
+
+findBind :: Module a -> Ident -> Maybe (Expr a)
+findBind m ident = head (catMaybes (map fromBind (moduleDecls m)))
+  where
+    fromBind :: Bind a -> Maybe (Expr a)
+    fromBind = \case
+      NonRec _ name expr | name == ident -> pure expr
+      _ -> Nothing
+
+eval :: Module Ann -> Expr Ann -> Eval Value
+eval _m = \case
+  Literal _ lit -> _
+  Constructor _ typeName ctorName fieldNames -> _
+  Accessor _ prop expr -> _
+  ObjectUpdate _ obj updates -> _
+  Abs _ arg body -> _
+  App _ func param -> _
+  Var _ ident -> _
+  Case _ exprs alts -> _
+  Let _ bindings body -> _
+
+evalEntryPoint :: Module Ann -> Ident -> Eval Value
+evalEntryPoint m entryPoint =
+  case findBind m entryPoint of
+    Just entry -> eval m entry
+    Nothing -> throwError (InvalidEntryPoint entryPoint)
+
+
+test :: FilePath -> IO ()
+test path = do
+  loadModule path >>= \case
+    Right (_v, m) -> print (runEval (evalEntryPoint m (Ident "spec")))
+    Left e -> putStrLn e

1194c56658907a79964fe542006a93f21e847a36
Oskar Wickström <oskar@wickstrom.tech>
2020-05-28 08:26:21 +0200

    Half assed interpreter

--- lib/WTP/PureScript.hs
+++ lib/WTP/PureScript.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PackageImports #-}
@@ -7,36 +12,44 @@
 
 module WTP.PureScript where
 
-import Protolude
-
-import Language.PureScript.CoreFn
-import Language.PureScript.Names
-import WTP.Element
-import Data.Scientific
-import Data.HashSet (HashSet)
-import Data.Vector (Vector)
-import Data.Version (Version)
-import qualified Data.Aeson.Types as JSON
+import Control.Lens hiding (op)
 import qualified Data.Aeson as JSON
-import Language.PureScript.CoreFn.FromJSON (moduleFromJSON)
+import qualified Data.Aeson.Types as JSON
 import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Generics.Sum (AsConstructor, _Ctor)
+import qualified Data.HashMap.Strict as HashMap
+import Data.HashMap.Strict (HashMap)
+import Data.HashSet (HashSet)
+import Data.Scientific
+import qualified Data.Text as Text
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+import Data.Version (Version)
+import Language.PureScript.AST (SourceSpan)
+import Language.PureScript.CoreFn
+import Language.PureScript.CoreFn.FromJSON (moduleFromJSON)
+import Language.PureScript.Names
+import Language.PureScript.PSString (PSString, decodeString)
+import Protolude
+import WTP.Element
 
 data Value
   = VNull
   | VBool Bool
   | VElement Element
   | VString Text
-  | VFunction Name Value
-  | VVar Name
+  | VChar Char
   | VNumber Scientific
-  | VSeq (Vector Value)
-  | VSet (HashSet Value)
-  deriving (Eq, Show)
-
+  | VArray (Vector Value)
+  | VObject (HashMap Text Value)
+  | VFunction (Ident, (Expr Ann))
+  deriving (Show, Generic)
 
 data EvalError
-  = Unexpected
-  | InvalidEntryPoint Ident
+  = UnexpectedError Text
+  | EntryPointNotDefined Ident
+  | NotInScope (Qualified Ident)
+  | InvalidString SourceSpan
   deriving (Eq, Show)
 
 type Eval a = Except EvalError a
@@ -51,8 +64,24 @@ loadModule path = do
     Just val ->
       case JSON.parse moduleFromJSON val of
         JSON.Success m -> pure (Right m)
-        JSON.Error e   -> pure (Left (toS e))
+        JSON.Error e -> pure (Left (toS e))
     Nothing -> pure (Left "Couldn't read CoreFn file.")
+
+require ::
+  forall (ctor :: Symbol) s t a b.
+  (KnownSymbol ctor, AsConstructor ctor s t a b, s ~ Value, t ~ Value, a ~ b) =>
+  Proxy ctor ->
+  Value ->
+  Eval b
+require (ctor :: Proxy ctor) v = case v ^? _Ctor @ctor of
+  Just x -> pure x
+  Nothing ->
+    throwError
+      ( UnexpectedError
+          ( "Expected value of type: "
+              <> Text.toLower (Text.drop 1 (Text.pack (symbolVal ctor)))
+          )
+      )
 
 findBind :: Module a -> Ident -> Maybe (Expr a)
 findBind m ident = head (catMaybes (map fromBind (moduleDecls m)))
@@ -62,27 +91,70 @@ findBind m ident = head (catMaybes (map fromBind (moduleDecls m)))
       NonRec _ name expr | name == ident -> pure expr
       _ -> Nothing
 
-eval :: Module Ann -> Expr Ann -> Eval Value
-eval _m = \case
-  Literal _ lit -> _
-  Constructor _ typeName ctorName fieldNames -> _
-  Accessor _ prop expr -> _
-  ObjectUpdate _ obj updates -> _
-  Abs _ arg body -> _
-  App _ func param -> _
-  Var _ ident -> _
-  Case _ exprs alts -> _
-  Let _ bindings body -> _
+evalString :: SourceSpan -> PSString -> Eval Text
+evalString ss s =
+  case decodeString s of
+    Just t -> pure t
+    Nothing -> throwError (InvalidString ss)
+
+type LocalEnv = HashMap Text Value
+
+eval :: LocalEnv -> Expr Ann -> Eval Value
+eval = go
+  where
+    go locals = \case
+      Literal (ss, _, _, _) lit -> case lit of
+        NumericLiteral n -> pure (either (VNumber . fromIntegral) (VNumber . realToFrac) n)
+        StringLiteral s -> VString <$> evalString ss s
+        CharLiteral c -> pure (VChar c)
+        BooleanLiteral b -> pure (VBool b)
+        ArrayLiteral xs -> VArray . Vector.fromList <$> traverse (go locals) xs
+        ObjectLiteral pairs -> do
+          pairs' <- for pairs $ \(field, value) ->
+            (,) <$> evalString ss field <*> go locals value
+          pure (VObject (HashMap.fromList pairs'))
+      Constructor _ typeName ctorName fieldNames ->
+        throwError (UnexpectedError "Constructors are not yet supported")
+      Accessor (ss, _, _, _) prop expr -> do
+        key <- evalString ss prop
+        obj <- require (Proxy @"VObject") =<< go locals expr
+        maybe
+          (throwError (UnexpectedError ("Key not present in object: " <> key)))
+          pure
+          (HashMap.lookup key obj)
+      ObjectUpdate (ss, _, _, _) expr updates -> do
+        obj <- require (Proxy @"VObject") =<< go locals expr
+        updates' <- for updates $ \(field, expr') ->
+          (,) <$> evalString ss field <*> go locals expr'
+        pure (VObject (obj <> HashMap.fromList updates'))
+      Abs _ arg body -> pure (VFunction (arg, body))
+      App _ func param -> do
+        (arg, body) <- require (Proxy @"VFunction") =<< go locals func
+        param' <- go locals param
+        go (locals <> HashMap.singleton (runIdent arg) param') body
+      Var _ qn@(Qualified Nothing localIdent) ->
+        maybe (throwError (NotInScope qn)) pure (HashMap.lookup (runIdent localIdent) locals)
+      Var _ qn@(Qualified _ _) ->
+        throwError (NotInScope qn)
+      Case _ exprs alts -> throwError (UnexpectedError "Case expressions are not yet supported")
+      Let _ bindings body -> do
+        let evalBinding = \case
+              NonRec _ name expr -> (runIdent name,) <$> go locals expr
+              Rec group -> throwError (UnexpectedError "Mutually recursive let bindings are not yet supported")
+        newEnv <- HashMap.fromList <$> traverse evalBinding bindings
+        go (locals <> newEnv) body
+        
+              
+      
 
 evalEntryPoint :: Module Ann -> Ident -> Eval Value
 evalEntryPoint m entryPoint =
   case findBind m entryPoint of
-    Just entry -> eval m entry
-    Nothing -> throwError (InvalidEntryPoint entryPoint)
+    Just entry -> eval mempty entry
+    Nothing -> throwError (EntryPointNotDefined entryPoint)
 
-
-test :: FilePath -> IO ()
-test path = do
+test :: FilePath -> Text -> IO ()
+test path name = do
   loadModule path >>= \case
-    Right (_v, m) -> print (runEval (evalEntryPoint m (Ident "spec")))
+    Right (_v, m) -> print (runEval (evalEntryPoint m (Ident name)))
     Left e -> putStrLn e

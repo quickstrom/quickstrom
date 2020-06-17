028953dd689a53e34e594a68679e8000b1131afc
Oskar Wickström <oskar@wickstrom.tech>
2020-06-17 22:11:12 +0200

    Remove unused pkgs

--- lib/WebCheck/Result.hs
+++ lib/WebCheck/Result.hs
@@ -1,26 +1,6 @@
 module WebCheck.Result where
 
-import Algebra.Heyting
-import Algebra.Lattice
-
 data Result
   = Accepted
   | Rejected
   deriving (Eq, Show)
-
-instance Lattice Result where
-  Accepted /\ Accepted = Accepted
-  _ /\ _ = Rejected
-
-  Accepted \/ _ = Accepted
-  Rejected \/ r = r
-
-instance BoundedJoinSemiLattice Result where
-  bottom = Rejected
-
-instance BoundedMeetSemiLattice Result where
-  top = Accepted
-
-instance Heyting Result where
-  Rejected ==> _ = Accepted
-  Accepted ==> r = r

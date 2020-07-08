01a4dfcf4c6b3c182446b145e2ead3d82ab0d405
Oskar Wickström <oskar@wickstrom.tech>
2020-07-08 06:30:09 +0200

    First super-basic spec for swedish tax calc

--- specs/other/SwedishTaxCalculation.spec.purs
+++ specs/other/SwedishTaxCalculation.spec.purs
@@ -3,17 +3,26 @@ module Spec where
 
 import WebCheck
 
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
+import Data.Symbol (SProxy(..))
 
 readyWhen :: Selector
-readyWhen = "app-root"
+readyWhen = "app-gdpr-modal"
 
 actions :: Actions
 actions = appFoci <> appClicks
   where
     appClicks = 
-      [ Tuple 5 (Click "[role=main] a")
+      [ Tuple 1 (Click "[role=main] a")
+      , Tuple 1 (Click "app-gdpr-modal #btn-center-confirm")
+      , Tuple 1 (Click "[role=main] form a")
+      , Tuple 1 (Click "[role=main] form input")
+      , Tuple 1 (Click "[role=main] form button")
       ]
     appFoci = [ Tuple 5 (Focus "input[type=text]") ]
 
-proposition = true
+proposition :: Boolean
+proposition = 
+  map _.classes (queryAll "#wizz-lon-efter-skatt li" { classes: attribute (SProxy :: SProxy "class") })
+  == [Just "active", Nothing]

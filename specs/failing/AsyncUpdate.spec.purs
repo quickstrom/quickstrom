0178f15978eef716442128d2ff16ae963d064ce5
Oskar Wickström <oskar@wickstrom.tech>
2020-07-26 22:10:07 +0200

    Add async page and spec

---
+++ specs/failing/AsyncUpdate.spec.purs
@@ -0,0 +1,32 @@
+module AsyncUpdateSpec where
+
+import WebCheck
+import Data.Maybe (Maybe(..))
+
+readyWhen :: String
+readyWhen = "button"
+
+actions :: Actions
+actions = clicks
+
+proposition :: Boolean
+proposition =
+  let
+    buttonIsEnabled = queryOne "button" { disabled } == Just { disabled: false }
+
+    disabledLaunchWithMessage msg =
+      queryOne ".message" { textContent } == Just { textContent: msg }
+        && not buttonIsEnabled
+
+    launch =
+      buttonIsEnabled
+        && next (disabledLaunchWithMessage "Missiles launched.")
+
+    impactOrNoImpact =
+      disabledLaunchWithMessage "Missiles launched."
+        && next
+            ( disabledLaunchWithMessage "Boom"
+                || disabledLaunchWithMessage "Missiles did not hit target."
+            )
+  in
+    buttonIsEnabled && always (launch || impactOrNoImpact)

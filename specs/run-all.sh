2fe5562ddf9ab3c570c9300e748ef6d4e652367c
Oskar Wickström <oskar@wickstrom.tech>
2020-08-14 23:13:31 +0200

    Run todomvc test

--- specs/run-all.sh
+++ specs/run-all.sh
@@ -1,28 +1,44 @@
 #!/usr/bin/env bash
 
+WEBCHECK=${WEBCHECK:-cabal run webcheck --}
+
 geckodriver > /dev/null 2>&1 &
 geckodriver_pid="$!"
 trap "kill $geckodriver_pid" EXIT
 
 echo "Geckodriver running ($geckodriver_pid)..."
 
+function run_test {
+    spec_file="$1"
+    uri="$2"
+    options="${3:---tests 10 --max-actions 100}"
+    expected_exit_code="$4"
+    echo "Specification: $spec_file"
+    echo "Origin: $uri"
+    echo "Options: $options"
+
+    # shellcheck disable=SC2086
+    $WEBCHECK "$spec_file" "$uri" $options
+    exit_code=$?
+
+    if [ $exit_code == "$expected_exit_code" ]; then
+        echo "Expected exit code ($expected_exit_code) was returned."
+    else 
+        echo "Expected exit code $expected_exit_code, but $exit_code was returned."
+        exit 1
+    fi
+}
+
 function run_group {
     dir="$1"
-    expected_exit_code="$2"
+    options="$2"
+    expected_exit_code="$3"
 
     for page in "$dir"/*.html; do
-        spec_file="${page%.*}.spec.purs"
-        echo -e "Specification: $spec_file\nOrigin: $page\n"
-        cabal run webcheck -- "$spec_file" "$page" --tests 5 --max-actions 50
-        exit_code=$?
-        if [ $exit_code == "$expected_exit_code" ]; then
-            echo "Expected exit code ($expected_exit_code) was returned."
-        else 
-            echo "Expected exit code $expected_exit_code, but $exit_code was returned."
-            exit 1
-        fi
+        run_test "${page%.*}.spec.purs" "$page" "$options" "$expected_exit_code"
     done
 }
 echo "Running WebCheck integration tests..."
-run_group "specs/passing" 0
-run_group "specs/failing" 1
+#run_group "specs/passing" "--tests 5 --max-actions 50" 0
+#run_group "specs/failing" "--tests 10 --max-actions 100 --shrink-levels=0" 1
+run_test "specs/other/TodoMVC.spec.purs" http://todomvc.com/examples/vue/ "--max-trailing-state-changes=0" 0

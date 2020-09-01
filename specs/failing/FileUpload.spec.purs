c06818866b8f1834bdebf4292b58498c744ad841
Oskar Wickström <oskar@wickstrom.tech>
2020-09-01 07:44:48 +0200

    Add failing file upload spec and html

---
+++ specs/failing/FileUpload.spec.purs
@@ -0,0 +1,25 @@
+module Specification where
+
+import Quickstrom
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+
+readyWhen = "button"
+
+actions = 
+  clicks <> [Tuple 10 (Click "input[type=file]")]
+
+proposition :: Boolean
+proposition =
+  let
+    initial = messageIs "" 
+    selectFile = selectedFile == Just "" && next (selectedFile /= Just "")
+    upload = selectedFile /= Just "" && next (messageIs "You uploaded:")
+  in
+    initial && always (selectFile || upload)
+
+selectedFile :: Maybe String
+selectedFile = _.value <$> queryOne "#file" { value }
+
+messageIs :: String -> Boolean
+messageIs message = queryOne ".message" { textContent } == Just { textContent: message }

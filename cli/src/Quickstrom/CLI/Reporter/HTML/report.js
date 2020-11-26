9872b67e9fb0da96b6b0d35262f1d3d41712e90f
owickstrom <oskar@wickstrom.tech>
2020-11-26 20:19:48 +0100

    Add markers

--- cli/src/Quickstrom/CLI/Reporter/HTML/report.js
+++ cli/src/Quickstrom/CLI/Reporter/HTML/report.js
@@ -1,13 +1,19 @@
-const traceElements = document.querySelectorAll(".trace li");
+const traceSteps = document.querySelectorAll(".trace li");
 const screenshotImg = document.querySelector(".screenshot img");
+const detailElements = document.querySelectorAll(".queries .elements li");
+const marker = document.querySelector(".marker");
 
-function activateTraceElement(element) {
+function activeTraceStep(element) {
     const traceIndex = element.dataset.traceIndex;
 
-    traceElements.forEach(other => other.classList.remove("active"));
+    traceSteps.forEach(other => other.classList.remove("active"));
     element.classList.add("active");
 
-    screenshotImg.src = `test-${traceIndex}.png`;
+    if (element.classList.contains("state")) {
+        screenshotImg.src = `test-${traceIndex}.png`;
+    } else {
+        screenshotImg.src = "";
+    }
 
     const details = document.querySelector(".details");
     details.classList.add("active");
@@ -22,9 +28,33 @@ function activateTraceElement(element) {
     }
 }
 
-traceElements.forEach(element => {
-    element.addEventListener("click", e => {
-        const traceIndex = element.dataset.traceIndex;
-        activateTraceElement(element);
+traceSteps.forEach(step => {
+    step.addEventListener("click", e => {
+        const traceIndex = step.dataset.traceIndex;
+        activeTraceStep(step);
+    });
+});
+
+detailElements.forEach(element => {
+    element.addEventListener("mouseenter", e => {
+        try {
+            const x = Number.parseInt(element.dataset.x);
+            const y = Number.parseInt(element.dataset.y);
+            const w = Number.parseInt(element.dataset.w);
+            const h = Number.parseInt(element.dataset.h);
+
+            if (w > 0 && h > 0) {
+                marker.classList.add("visible");
+                marker.style.left = `${x}px`;
+                marker.style.top = `${y}px`;
+                marker.style.width = `${w}px`;
+                marker.style.height = `${h}px`;
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    });
+    element.addEventListener("mouseleave", e => {
+        marker.classList.remove("visible");
     });
 });

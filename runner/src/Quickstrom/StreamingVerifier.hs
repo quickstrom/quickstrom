980ea6c354858113dfc7a9ac3474ab918c0819a1
owickstrom <oskar@wickstrom.tech>
2021-02-18 22:08:43 +0100

    More WIP

--- runner/src/Quickstrom/StreamingVerifier.hs
+++ runner/src/Quickstrom/StreamingVerifier.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Quickstrom.StreamingVerifier where
 
-import Numeric.Natural
 import Algebra.Heyting
 import Algebra.Lattice
-import Quickstrom.Prelude hiding (State, and, negate, not, show)
 import GHC.Show
+import Numeric.Natural
+import Quickstrom.Prelude hiding (State, and, negate, not, show)
 
 -- * Language
 
@@ -38,6 +39,7 @@ instance Heyting a => Heyting (Certainty a) where
 type Result = Certainty Bool
 
 type State = Char
+
 type Trace = [State]
 
 data Formula
@@ -46,12 +48,12 @@ data Formula
   | Atomic (State -> Bool)
   | And Formula Formula
   | Or Formula Formula
-  -- | strong next
-  | Next Formula
-  -- | weak next
-  | WNext Formula
-  -- | demanding next
-  | DNext Formula
+  | -- | strong next
+    Next Formula
+  | -- | weak next
+    WNext Formula
+  | -- | demanding next
+    DNext Formula
 
 instance Show Formula where
   show = \case
@@ -84,8 +86,17 @@ eventually 0 f = f \/ Next (eventually 0 f)
 eventually n f = f \/ DNext (eventually (pred n) f)
 
 instance Lattice Formula where
-  (/\) = And
-  (\/) = Or
+  Absurd /\ _ = Absurd
+  _ /\ Absurd = Absurd
+  Trivial /\ x = x
+  x /\ Trivial = x
+  x /\ y = And x y
+
+  Absurd \/ x = x
+  x \/ Absurd = x
+  Trivial \/ _ = Trivial
+  _ \/ Trivial = Trivial
+  x \/ y = Or x y
 
 instance BoundedMeetSemiLattice Formula where
   top = Trivial
@@ -97,39 +108,46 @@ instance BoundedJoinSemiLattice Formula where
 
 step :: Formula -> State -> Formula
 step Trivial _ = Trivial
-step Absurd _ = Trivial
+step Absurd _ = Absurd
 step (Atomic a) s = if a s then Trivial else Absurd
-step (And f1 f2) s =
-  case step f1 s of
-    Trivial -> step f2 s
-    Absurd -> Absurd
-    r1 ->
-      case step f2 s of
-        Trivial -> Trivial
-        Absurd -> Absurd
-        r2 -> r1 /\ r2
-step (Or f1 f2) s = step f1 s \/ step f2 s -- TODO: ???
+step (And f1 f2) s = step f1 s /\ step f2 s
+step (Or f1 f2) s = step f1 s \/ step f2 s
 step (Next f) _ = f
 step (WNext f) _ = f
 step (DNext f) _ = f
 
+requiresMoreStates :: Formula -> Bool
+requiresMoreStates = \case
+  Trivial -> False
+  Absurd -> False
+  Atomic {} -> False
+  (And f1 f2) -> requiresMoreStates f1 \/ requiresMoreStates f2
+  (Or f1 f2) -> requiresMoreStates f1 \/ requiresMoreStates f2
+  Next {} -> False
+  WNext {} -> False
+  DNext {} -> True
+
+stepRequired :: Formula -> Trace -> Formula
+stepRequired f (x:xs) | requiresMoreStates f = stepRequired (step f x) xs
+stepRequired f _ = f
+
+-- TODO
+stepResidual :: Formula -> Trace -> Formula
+stepResidual f (x:xs) = stepRequired (step f x) xs
+stepResidual f _ = f
+
+compute :: Formula -> Maybe Result
+compute = \case
+  Trivial -> Just (Definitely True)
+  Absurd -> Just (Definitely False)
+  Atomic {} -> Nothing
+  Next {} -> Just (Probably False)
+  WNext {} -> Just (Probably True)
+  DNext {} -> Nothing
+  And p q -> liftA2 (/\) (compute p) (compute q)
+  Or p q -> liftA2 (\/) (compute p) (compute q)
+
 verify :: Formula -> Trace -> Maybe Result
-verify = go 
-  where
-    go = \case
-      Trivial -> const (Just (Definitely True))
-      Absurd -> const (Just (Definitely False))
-      a@Atomic{} -> \case
-        [] -> Nothing
-        x' : xs' -> go (step a x') xs'
-      Next p -> \case
-        [] -> Just (Probably False)
-        x' : xs' -> go (step p x') xs'
-      WNext p -> \case
-        [] -> Just (Probably True)
-        x' : xs' -> go (step p x') xs'
-      DNext p -> \case
-        [] ->  Nothing
-        x' : xs' -> go (step p x') xs'
-      And p q -> \xs' -> liftA2 (/\) (go p xs') (go q xs')
-      Or p q -> \xs' -> liftA2 (\/) (go p xs') (go q xs')
+verify f t = 
+  let f' = stepRequired f t
+  in compute f'

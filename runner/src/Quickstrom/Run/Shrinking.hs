750f1dce84317d1f63aa3684573cdde546fccab9
owickstrom <oskar@wickstrom.tech>
2021-03-15 14:27:30 +0100

    Try more aggressive shrink strategies first

--- runner/src/Quickstrom/Run/Shrinking.hs
+++ runner/src/Quickstrom/Run/Shrinking.hs
@@ -1,29 +1,40 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Quickstrom.Run.Shrinking where
 
-import Quickstrom.Prelude
-
-import qualified Pipes
-import Data.Tree
 import Control.Lens
 import Control.Lens.Extras (is)
+import Data.String (String)
+import Data.Tree
+import qualified Pipes
+import Quickstrom.Prelude
 
 newtype Shrink a = Shrink a
   deriving (Eq, Show)
 
-shrinkForest :: (a -> [a]) -> a -> Forest (Shrink a)
-shrinkForest shrink = go
+shrinkForest :: (Ord strategy, Enum strategy, Bounded strategy) => (strategy -> a -> [a]) -> a -> Forest (Shrink a)
+shrinkForest shrink = go [minBound .. maxBound]
   where
-    go = map (\x -> Node (Shrink x) (go x)) . shrink
+    go [] _ = []
+    go strategies x =
+      [ Node (Shrink x') (go (filter (>= strategy) strategies) x')
+        | strategy <- strategies,
+          x' <- shrink strategy x
+      ]
 
-shrinkActions :: [a] -> [[a]]
-shrinkActions [] = []
-shrinkActions as = filter (not . null) ([first75] <> maybeToList (initMay as))
+data ListStrategy = Half | Quarter | Single
+  deriving (Eq, Show, Ord, Enum, Bounded)
+
+shrinkActions :: ListStrategy -> [a] -> [[a]]
+shrinkActions _ [] = []
+shrinkActions strategy as = filter (not . null) $ case strategy of
+  Half -> [firstPercent 0.5 as]
+  Quarter -> [firstPercent 0.75 as]
+  Single -> maybeToList (initMay as)
   where
-    first75 = take (floor @Double (fromIntegral (length as) * 0.75)) as
+    firstPercent :: Double -> [a] -> [a]
+    firstPercent p = take (floor (fromIntegral (length as) * p))
 
 traverseShrinks :: Monad m => (Shrink a -> m b) -> Prism' b x -> Forest (Shrink a) -> Pipes.Producer b m ()
 traverseShrinks test failure = go
@@ -33,6 +44,9 @@ traverseShrinks test failure = go
       Node x xs : rest -> do
         r <- lift (test x)
         Pipes.yield r
-        when (is failure r) do
+        when (is failure r) $
           go xs
         go rest
+
+drawShrinkForest :: Show a => Forest (Shrink a) -> String
+drawShrinkForest = drawForest . map (map show)

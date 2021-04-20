05a60db9b797e47629b18880694e69ee127735d7
Oskar Wickström <oskar@wickstrom.tech>
2021-04-20 08:32:30 +0200

    Use latest python 3

--- docs/default.nix
+++ docs/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import ../nixpkgs.nix { } }:
 let
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-  sphinx-env = pkgs.python37.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
+  sphinx-env = pkgs.python3.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
   dependencies = [ sphinx-env pkgs.texlive.combined.scheme-basic pkgs.graphviz ];
   site = pkgs.stdenv.mkDerivation {
     inherit src;

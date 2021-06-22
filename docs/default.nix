57f88db3e9873b9d27e2fc459b44467b49f788a0
owickstrom <oskar@wickstrom.tech>
2021-06-22 12:43:48 +0200

    Lint for docs

--- docs/default.nix
+++ docs/default.nix
@@ -1,10 +1,11 @@
 { pkgs ? import ../nixpkgs.nix { } }:
 let
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+  quickstrom = (import ../. {}).quickstrom;
   sphinx-env =
     pkgs.python3.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
   dependencies =
-    [ sphinx-env pkgs.texlive.combined.scheme-basic pkgs.graphviz ];
+    [ sphinx-env pkgs.texlive.combined.scheme-basic pkgs.graphviz quickstrom ];
   site = pkgs.stdenv.mkDerivation {
     inherit src;
     name = "docs";

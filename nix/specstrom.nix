bddbc7161ed902adb92c4bc70ccb6eb4cd39c394
Oskar Wickström <oskar@wickstrom.tech>
2022-11-08 22:20:55 +0100

    Handle errors from Specstrom better, display in output

--- nix/specstrom.nix
+++ nix/specstrom.nix
@@ -1,3 +1,3 @@
 (import (fetchTarball
-  "https://github.com/quickstrom/specstrom/archive/71835fce0b4d9fed7ab64355af3f5e8900a0e820.tar.gz") {
+  "https://github.com/quickstrom/specstrom/archive/48b23a901d22ef97ebf40541066d42558f88d19a.tar.gz") {
   }).specstrom

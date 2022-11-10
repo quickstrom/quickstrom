989953dddb17cb484998c1ae603585358805f12c
Oskar Wickström <oskar@wickstrom.tech>
2022-11-10 06:50:55 +0100

    Fix conf error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,8 +99,8 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+
 html_theme_options = {
-    'html_baseurl': 'https://quickstrom.io/',
     # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': False,
     'display_version': True,
@@ -115,6 +115,8 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+
+html_baseurl = "https://quickstrom.io/"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

77d304eb6e647428fa68e827daed402629a9bffc
Oskar Wickström <oskar@wickstrom.tech>
2022-11-11 08:05:36 +0100

    Delete old case studies

--- case-studies/runtimes/shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? (import ../../nix/nixpkgs.nix)
-}:
-let
-  py = pkgs.python39.withPackages (p: [ p.jupyter p.ipython p.pandas p.pip p.notebook ]);
-in pkgs.mkShell {
-  buildInputs = [py];
-}

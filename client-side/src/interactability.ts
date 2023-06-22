291c7570427dbb3c0bd2ea0bddac899457435243
Oskar Wickström <oskar@wickstrom.tech>
2023-06-22 10:39:02 +0200

    Check that element is "clickable" to be interactable

--- client-side/src/interactability.ts
+++ client-side/src/interactability.ts
@@ -1,5 +1,24 @@
-import { isElementVisible } from "./visibility";
+import {isElementVisible} from "./visibility";
+
+function isChildOf(el: HTMLElement, parent: HTMLElement): boolean {
+    if (el === parent) {
+        return true;
+    } else if (!!el.parentElement) {
+        return isChildOf(el.parentElement, parent);
+    } else {
+        return false;
+    }
+}
+
+function wouldReceiveClick(element: HTMLElement) {
+    // check if clicking at the center of the element would hit it
+    const rect = element.getBoundingClientRect();
+    const x = rect.left + rect.width / 2;
+    const y = rect.top + rect.height / 2;
+    const hitElement = document.elementFromPoint(x, y);
+    return hitElement && isChildOf(hitElement as HTMLElement, element);
+}
 
 export function isElementInteractable(element: HTMLElement) {
-    return isElementVisible(element) && window.getComputedStyle(element).pointerEvents !== "none";
+    return isElementVisible(element) && window.getComputedStyle(element).pointerEvents !== "none" && wouldReceiveClick(element);
 }

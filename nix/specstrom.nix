5446ab513f85d9ddd878687b087c95ad11f44c28
Oskar Wickström <oskar@wickstrom.tech>
2023-06-28 16:30:55 +0200

    Restore specstrom import

--- nix/specstrom.nix
+++ nix/specstrom.nix
@@ -1,4 +1,3 @@
-#(import (fetchTarball
-#  "https://github.com/quickstrom/specstrom/archive/d87e6ae4855aa9abf8a45d8aaa337d8d453ed4fa.tar.gz") {
-#  }).specstrom
-(import /Users/owi/projects/specstrom {}).specstrom
+(import (fetchTarball
+ "https://github.com/quickstrom/specstrom/archive/d87e6ae4855aa9abf8a45d8aaa337d8d453ed4fa.tar.gz") {
+ }).specstrom
